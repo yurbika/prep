76a02fc0f94320edbe2cd70be9d94cf37b384f13
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-06-18 19:03:15 +0200

    adding bouncing

--- fractal-tree.js
+++ fractal-tree.js
@@ -1,16 +1,21 @@
 import React from "react";
+import gsap from "gsap";
+import { Elastic } from "gsap/gsap-core";
 
-function rnd(min, max) {
+const rnd = (min, max) => {
   return Math.random() * (max - min) + min;
-}
+};
 
-var mult = rnd(0.01, 0.1);
-var totalLevel = 6;
+const mult = rnd(0.01, 0.1);
+const totalLevel = 3;
+let sway = 0;
 
 //creates position of every group which stacks on eachother
-const posOfG = ({ gen, ang }) => {
+const posOfG = ({ len, ang, gen }) => {
   //rotation in deg
-  const rotation = ang + (((Math.sin(mult) * Math.PI) / 128) * 180) / Math.PI;
+  if (gen === 0) sway++;
+  const rotation =
+    ang + (((Math.sin(sway * mult) * Math.PI) / 128) * 180) / Math.PI;
   const style = {
     id: "gen" + gen,
     //bad practices but i am lazy
@@ -89,12 +94,69 @@ const Branch = ({ len, ang, gen }) => {
 };
 
 class Tree extends React.Component {
+  componentDidMount() {
+    var test = Elastic.easeOut.config(1, 0.3);
+    gsap.timeline().from("#gen0", {
+      duration: 3.4,
+      transformOrigin: "50% bottom",
+      scaleY: 0,
+      ease: test,
+    });
+    gsap.timeline().from("#gen1", {
+      duration: 0.4,
+      transformOrigin: "50% bottom",
+      scaleY: 0,
+      ease: test,
+    });
+    gsap.timeline().from("#gen2", {
+      duration: 0.4,
+      transformOrigin: "50% bottom",
+      scaleY: 0,
+      ease: test,
+    });
+    gsap.timeline().from("#gen3", {
+      duration: 0.4,
+      transformOrigin: "50% bottom",
+      scaleY: 0,
+      ease: test,
+    });
+  }
+
+  componentDidUpdate() {
+    var test = Elastic.easeOut.config(1, 0.3);
+    gsap.timeline().from("#gen0", {
+      duration: 3,
+      transformOrigin: "50% bottom",
+      scaleY: 0,
+      ease: test,
+    });
+    gsap.timeline().from("#gen1", {
+      duration: 0.6,
+      transformOrigin: "50% bottom",
+      scaleY: 0,
+      ease: test,
+    });
+    gsap.timeline().from("#gen2", {
+      duration: 0.6,
+      transformOrigin: "50% bottom",
+      scaleY: 0,
+      ease: test,
+    });
+    gsap.timeline().from("#gen3", {
+      duration: 0.6,
+      transformOrigin: "50% bottom",
+      scaleY: 0,
+      ease: test,
+    });
+  }
+
   render() {
     return (
       <svg
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="-400,-500,800,650"
+        onClick={() => this.forceUpdate()}
       >
         <Branch len={80} ang={0} gen={0} />
       </svg>
