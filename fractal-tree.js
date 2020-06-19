40865a34ee461160369f916b480fd1240bc7b4c9
Berkay Yurdagül <yurdaguel.b@gmail.com>
2020-06-19 12:29:53 +0200

    better spawn animation

--- fractal-tree.js
+++ fractal-tree.js
@@ -23,7 +23,7 @@ const posOfG = ({ len, ang, gen }) => {
     transform:
       gen === 0
         ? `
-        ${"scale(" + 2.5 + ")"} rotate(${rotation},0,0)`
+        ${"scale(" + 3.5 + ")"} rotate(${rotation},0,0)`
         : //translate values are depending on the path values
           `translate(0.35,-28.8) ${"scale(0.87)"} rotate(${rotation},0,0)`,
   };
@@ -40,7 +40,7 @@ const Branch = ({ len, ang, gen }) => {
       Branch,
       Object.assign(
         { len: len * rnd(0.8, 0.99) },
-        { ang: rnd(0, 37) },
+        { ang: rnd(0, 50) },
         { gen: gen + 1 },
         { key: rnd(0, 1110004239042390482034820480293429034) }
       ),
@@ -63,7 +63,7 @@ const Branch = ({ len, ang, gen }) => {
       Branch,
       Object.assign(
         { len: len * rnd(0.8, 0.99) },
-        { ang: rnd(0, -37) },
+        { ang: rnd(0, -50) },
         { gen: gen + 1 },
         { key: rnd(0, 1110004239042390482034820480293429034) }
       ),
@@ -102,62 +102,73 @@ class Tree extends React.Component {
   componentDidMount() {
     var test = Elastic.easeOut.config(1, 0.3);
     gsap.timeline().from("#gen0", {
-      duration: rnd(3.5, 3.8),
+      duration: 2,
       transformOrigin: "50% bottom",
       scaleY: 0,
       ease: test,
     });
     gsap.timeline().from("#gen0 #path0", {
-      duration: 0.6,
+      duration: 0.1,
       transformOrigin: "50% bottom",
       scaleY: 0,
     });
-
     for (let i = 1; i < totalLevel + 1; i++) {
-      gsap.timeline().from("#gen" + i + " #path" + i, {
-        delay: 0.6 + i,
-        duration: 0.6,
-        transformOrigin: "50% bottom",
-        scaleY: 0,
-      });
-    }
-
-    for (let i = 1; i < totalLevel + 1; i++) {
-      gsap.timeline().from("#gen" + i, {
-        duration: rnd(0.7, 0.8),
-        transformOrigin: "50% bottom",
-        scaleY: 0,
-        ease: test,
-      });
+      gsap
+        .timeline()
+        .timeScale(10)
+        .from("#gen" + i, {
+          delay: 0.4,
+          duration: 0.6,
+          transformOrigin: "50% bottom",
+          scaleY: 0.5,
+          ease: test,
+        });
+      gsap
+        .timeline()
+        .timeScale(15)
+        .from("#gen" + i + " #path" + i, {
+          delay: 0.4 + i,
+          duration: 0.6,
+          transformOrigin: "50% bottom",
+          scaleY: 0,
+        });
     }
   }
 
   componentDidUpdate() {
     var test = Elastic.easeOut.config(1, 0.3);
     gsap.timeline().from("#gen0", {
-      duration: 3,
+      duration: 2,
       transformOrigin: "50% bottom",
       scaleY: 0,
       ease: test,
     });
-    gsap.timeline().from("#gen1", {
-      duration: 0.6,
+    gsap.timeline().from("#gen0 #path0", {
+      duration: 0.1,
       transformOrigin: "50% bottom",
       scaleY: 0,
-      ease: test,
     });
-    gsap.timeline().from("#gen2", {
-      duration: 0.6,
-      transformOrigin: "50% bottom",
-      scaleY: 0,
-      ease: test,
-    });
-    gsap.timeline().from("#gen3", {
-      duration: 0.6,
-      transformOrigin: "50% bottom",
-      scaleY: 0,
-      ease: test,
-    });
+    for (let i = 1; i < totalLevel + 1; i++) {
+      gsap
+        .timeline()
+        .timeScale(10)
+        .from("#gen" + i, {
+          delay: 0.4,
+          duration: 0.6,
+          transformOrigin: "50% bottom",
+          scaleY: 0.5,
+          ease: test,
+        });
+      gsap
+        .timeline()
+        .timeScale(15)
+        .from("#gen" + i + " #path" + i, {
+          delay: 0.4 + i,
+          duration: 0.6,
+          transformOrigin: "50% bottom",
+          scaleY: 0,
+        });
+    }
   }
 
   render() {
@@ -165,7 +176,7 @@ class Tree extends React.Component {
       <svg
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="-400,-500,800,650"
+        viewBox="-400,-550,800,650"
         onClick={() => this.forceUpdate()}
       >
         <Branch len={80} ang={0} gen={0} />
