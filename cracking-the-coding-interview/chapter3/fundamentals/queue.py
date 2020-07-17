a54359c82affca01ba93ca80d966cbdc3bcf5ae7
Berkay <yurdaguel.b@gmail.com>
2020-07-17 21:05:43 +0200

    queue chapter 3 fundamentals

---
+++ cracking-the-coding-interview/chapter3/fundamentals/queue.py
@@ -0,0 +1,65 @@
+#last in => first out
+
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self._next = None
+
+    def __repr__(self):
+        return self.val
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def __repr__(self):
+        node = self.head
+        nodes=[]
+
+        while node is not None:
+            nodes.append(f"{node.val}")
+            node = node.next
+
+        return "->".join(nodes)    
+    
+    def is_empty(self):
+        return self.head is None
+    
+    def remove(self):
+        n = self.head
+        if self.is_empty():
+            return None
+        while n.next.next is not None:
+            n = n.next
+        temp = n.next.val
+        n.next = None
+        return temp
+
+
+
+    def push(self,val):
+        temp = self.head
+        self.head = Node(val)
+        self.head.next = temp
+        self.size += 1
+        
+    def peek(self):
+        if self.is_empty():
+            return None
+
+        return self.head.val
+
+
+test = Queue()
+
+
+test.push(1)
+test.push(2)
+test.push(3)
+test.push(4)
+
+
+print(test)
+
+print(test.remove(),test)
