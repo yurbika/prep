2ae550900e35066a9f70aaf52d7fec359cf3eeae
Berkay <yurdaguel.b@gmail.com>
2020-07-19 09:29:02 +0200

    leetcode #1431

---
+++ leetcode/1431.Kids-With-the-Greatest-Number-of-Candies.py
@@ -0,0 +1,6 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+                return [i + extraCandies>=max(candies) for i in candies]
+
+# JavaScript
+# var kidsWithCandies = (candies, extraCandies) => candies.map(i => i+extraCandies>=Math.max(...candies)?true:false)    
