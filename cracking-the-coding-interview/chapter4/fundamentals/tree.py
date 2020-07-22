bf76a4011d0149279283c1e6ba1ddfb58c0a2cc4
Berkay <yurdaguel.b@gmail.com>
2020-07-22 10:20:42 +0200

    tree print in-/pre-/postorder chapter4

--- cracking-the-coding-interview/chapter4/fundamentals/tree.py
+++ cracking-the-coding-interview/chapter4/fundamentals/tree.py
@@ -4,16 +4,36 @@ class Tree:
         self.left  = left
         self.right = right
 
-    def print_tree(self,side,tree):
+    def print_tree_inorder(self,tree):
         if tree == None: 
             return
-        print(side,tree.data),
-        self.print_tree("left: ",tree.left)
-        self.print_tree("right: ",tree.right)
+        self.print_tree_inorder(tree.left)
+        print(tree.data)
+        self.print_tree_inorder(tree.right)
+
+    def print_tree_postorder(self,tree):
+        if tree == None: 
+            return
+        self.print_tree_postorder(tree.left)
+        self.print_tree_postorder(tree.right)
+        print(tree.data)
+
+    
+    def print_tree_preorder(self,tree):
+        if tree == None: 
+            return
+        print(tree.data)
+        self.print_tree_preorder(tree.left)
+        self.print_tree_preorder(tree.right)
 
     def __str__(self):
         return str(self.data)
 
 tree = Tree(1, Tree(2,Tree(33),Tree(21)), Tree(3))
 
-tree.print_tree("root: ",tree)
+print("inorder:")
+tree.print_tree_inorder(tree)
+print("preorder:")
+tree.print_tree_preorder(tree)
+print("postorder:")
+tree.print_tree_postorder(tree)
