ddf36e0b7db19b3af32465e71054457a0f421b44
Berkay <yurdaguel.b@gmail.com>
2020-07-28 15:37:26 +0200

    question3 chapter5

--- cracking-the-coding-interview/chapter5/flip-bit-to-win.py
+++ cracking-the-coding-interview/chapter5/flip-bit-to-win.py
@@ -44,3 +44,40 @@ def flip(num):
 print(flip(sys.maxsize))
 print("\n")
 print(flip(round(math.exp(500))))
+
+# solution 2 with O(1) space
+
+
+def shiftToWin(num):
+    if num == 0:
+        return 1
+
+    if int(bin(~num).split('0b1')[1], base=2) == 0:
+        print(num)
+        return len(bin(num).split('0b')[1])
+
+    cnt = 0
+    currBit = 0
+    flipped = False
+    solution = 0
+    while num:
+        currBit = num & 1
+        num = num >> 1
+
+        if currBit == 1:
+            cnt += 1
+        elif currBit == 0 and not flipped:
+            cnt += 1
+            flipped = True
+        else:
+            flipped = False
+            if cnt > solution:
+                solution = cnt
+            cnt = 1
+        if cnt > solution:
+            solution = cnt
+    return solution
+
+
+print("\n")
+print(shiftToWin(sys.maxsize))
