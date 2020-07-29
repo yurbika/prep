228a6e8fef361d6248a0827db72962b50e0e0752
Berkay <yurdaguel.b@gmail.com>
2020-07-29 21:11:22 +0200

    question1 chapter8 memo

--- cracking-the-coding-interview/chapter8/triple-step.py
+++ cracking-the-coding-interview/chapter8/triple-step.py
@@ -14,3 +14,21 @@ def step(n):
 
 
 print(step(n))
+
+
+def stepM(n, memo):
+    if n < 0:
+        return 0
+
+    if n == 0:
+        return 1
+
+    if memo[n] > 0:
+        return memo[n]
+
+    else:
+        memo[n] = stepM(n-1, memo) + stepM(n-2, memo) + stepM(n - 3, memo)
+        return memo[n]
+
+
+print(stepM(n, [0]*(n+1)))
