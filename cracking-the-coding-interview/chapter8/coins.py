f257974b309f92a00a7f3bc088cd3b378d82cd8e
Berkay <yurdaguel.b@gmail.com>
2020-08-03 11:39:30 +0200

    question11 chapter8

--- cracking-the-coding-interview/chapter8/coins.py
+++ cracking-the-coding-interview/chapter8/coins.py
@@ -1,54 +1,64 @@
-n = 27
+n = 50
 
 
 def coins(n, penny, currentArr, memo):
     if n == 0:
-        if penny != 0:
-            if penny == 1:
-                currentArr[0] += 1
-            if penny == 5:
-                currentArr[1] += 1
-            if penny == 10:
-                currentArr[2] += 1
-            if penny == 25:
-                currentArr[3] += 1
-        temp = currentArr[:]
-        if temp not in memo:
-            memo.append(temp)
-        return memo
-
-    if n < 0:
-        n += penny
-        return memo
-
-    if penny != 0:
         if penny == 1:
             currentArr[0] += 1
-        if penny == 5:
+        elif penny == 5:
             currentArr[1] += 1
-        if penny == 10:
+        elif penny == 10:
             currentArr[2] += 1
-        if penny == 25:
+        elif penny == 25:
             currentArr[3] += 1
+        if currentArr not in memo:
+            memo.append(currentArr[:])
+            return memo, currentArr
+        else:
+            return memo, currentArr
 
-    memo = coins(n-1, 1, currentArr, memo)
-    if currentArr[0] > 0:
-        currentArr[0] -= 1
+    if n < -1:
+        n += penny
+        if penny == 1:
+            currentArr[0] -= 1
+        elif penny == 5:
+            currentArr[1] -= 1
+        elif penny == 10:
+            currentArr[2] -= 1
+        elif penny == 25:
+            currentArr[3] -= 1
+        return memo, currentArr
 
-    memo = coins(n-5, 5, currentArr, memo)
-    if currentArr[1] > 0 and (n-5) <= 0:
-        currentArr[1] -= 1
+    if penny == 1:
+        currentArr[0] += 1
+    elif penny == 5:
+        currentArr[1] += 1
+    elif penny == 10:
+        currentArr[2] += 1
+    elif penny == 25:
+        currentArr[3] += 1
 
-    memo = coins(n-10, 10, currentArr, memo)
-    if currentArr[2] > 0 and (n-10) <= 0:
-        currentArr[2] -= 1
+    if n >= 25:
+        memo, currentArr = coins(n-25, 25, currentArr, memo)
+        if currentArr[3] > 0:
+            currentArr[3] -= 1
 
-    memo = coins(n-25, 25, currentArr, memo)
-    if currentArr[3] > 0 and (n-25) <= 0:
-        currentArr[3] -= 1
+    if n >= 10:
+        memo, currentArr = coins(n-10, 10, currentArr, memo)
+        if currentArr[2] > 0:
+            currentArr[2] -= 1
 
-    return memo
+    if n >= 5:
+        memo, currentArr = coins(n-5, 5, currentArr, memo)
+        if currentArr[1] > 0:
+            currentArr[1] -= 1
+
+    if n >= 1:
+        memo, currentArr = coins(n-1, 1, currentArr, memo)
+        if currentArr[0] > 0:
+            currentArr[0] -= 1
+    return memo, currentArr
 
 
-arr = coins(n, 0, [0]*4, [])
+arr = coins(n, 0, [0]*4, [])[0]
 print(arr, "\n", len(arr))
