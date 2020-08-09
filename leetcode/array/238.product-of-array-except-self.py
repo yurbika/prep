eb4e0a8ea6b13da000fe88cf78c2b756dd50abad
Berkay <yurdaguel.b@gmail.com>
2020-08-09 11:29:51 +0200

    leetcode238

--- leetcode/array/238.product-of-array-except-self.py
+++ leetcode/array/238.product-of-array-except-self.py
@@ -1,22 +1,13 @@
-import functools
-
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        product = functools.reduce(lambda i, j: i*j, nums)
-        if product == 0 and nums.count(0) > 1:
-            return [0]*len(nums)
+        ans = [1] * len(nums)
+        left = 1
+        right = 1
 
-        elif product == 0 and nums.count(0) == 1:
-            temp = []
-            temp = nums[:nums.index(0)]+nums[nums.index(0)+1:]
-            product = functools.reduce(lambda i, j: i*j, temp)
-            index = nums.index(0)
-            nums = [0 for i in range(len(nums))]
-            nums[index] = product
+        for i in range(len(nums)):
+            ans[i] *= left
+            ans[-1-i] *= right
+            left *= nums[i]
+            right *= nums[-1-i]
 
-        else:
-            for i in range(len(nums)):
-                if nums[i] != 0:
-                    nums[i] = round(product/nums[i])
-        return nums
+        return ans
