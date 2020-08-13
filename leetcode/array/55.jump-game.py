77c90cadb572cb27a7865d2dff8cfee58ed16970
Berkay <yurdaguel.b@gmail.com>
2020-08-13 11:19:20 +0200

    leetcode55#

--- leetcode/array/55.jump-game.py
+++ leetcode/array/55.jump-game.py
@@ -1,18 +1,11 @@
-def canJump(nums):
-    if len(nums) == 1:
-        return True
-    if nums[-1] == 0:
-        return True
-    for i in range(len(nums)):
-        if i == len(nums) - 1 and (nums[i] == len(nums)+1 or nums[i] % (len(nums)+1) == 0):
-            return True
-        if i == 0 and nums[i] % (len(nums)-1) == 0 and nums[i] != 0:
-            return True
-        if i == len(nums) - 1:
-            i = -1
-        if nums[i] + i + 1 == len(nums):
-            return True
-    return False
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        maxValue = 0
 
-
-print(canJump([0, 2, 3]))
+        for i in range(len(nums)):
+            if maxValue < i:
+                return False
+            if maxValue >= len(nums)-1:
+                return True
+            maxValue = max(maxValue, i + nums[i])
+        return True
