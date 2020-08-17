e72929ec22e534a3f18be96aed9d2ba25a2950ea
Berkay <yurdaguel.b@gmail.com>
2020-08-17 20:30:22 +0200

    cleaning up

--- leetcode/1480.Running-Sum-of-1d-Array.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        temp = []
-        for i in range(len(nums)):
-            temp.append(sum(nums))
-            nums.pop(len(nums)-1)
-        
-        temp.reverse()
-        return temp
