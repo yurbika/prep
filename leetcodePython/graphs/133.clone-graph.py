a0ac48e8159edaf3bf27daee00c2f9e2684cf301
Berkay <yurdaguel.b@gmail.com>
2020-08-27 20:39:14 +0200

    leetcode133

---
+++ leetcodePython/graphs/133.clone-graph.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return node
+
+        queue = [node]
+        visited = {}
+        visited[node] = Node(node.val, [])
+
+        while queue:
+            curNode = queue.pop(0)
+            for neighbor in curNode.neighbors:
+                if neighbor not in visited:
+                    visited[neighbor] = Node(neighbor.val, [])
+                    queue.append(neighbor)
+                visited[curNode].neighbors.append(visited[neighbor])
+
+        return visited[node]
