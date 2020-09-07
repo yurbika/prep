ac1a3e7031b77bdbcbd03254a9318ce94ab6fb9e
Berkay <yurdaguel.b@gmail.com>
2020-09-07 23:32:40 +0200

    leetcode154

---
+++ leetcodeJS/array/154.find-minimum-in-rotated-sorted-array-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((right + left) / 2);
+
+    if (nums[mid] < nums[right]) right = mid;
+    else if (nums[mid] > nums[right]) left = mid + 1;
+    else right--;
+  }
+  return nums[left];
+};
