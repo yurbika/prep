242f24653660d158e406326c6745dd33af9e98a0
Berkay <yurdaguel.b@gmail.com>
2020-09-13 18:55:16 +0200

    leetcode905

--- leetcodeJS/array/905.sort-array-by-parity.js
+++ leetcodeJS/array/905.sort-array-by-parity.js
@@ -12,3 +12,20 @@ var sortArrayByParity = function (A) {
   }
   return solution;
 };
+
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParity = function (A) {
+  if (A.length === 0 || A.length === 1) return A;
+  let left = 0;
+  let right = A.length - 1;
+  while (left < right) {
+    if (A[left] % 2) [A[left], A[right]] = [A[right], A[left]];
+
+    if (A[left] % 2 === 0) left++;
+    if (A[right] % 2 === 1) right--;
+  }
+  return A;
+};
